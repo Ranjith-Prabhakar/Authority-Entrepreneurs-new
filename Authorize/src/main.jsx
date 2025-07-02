65749cc3f138d7fea0d4eb47fcc8749a82000448
Ranjith <ranjithp2294@gmail.com>
2025-07-02 13:11:12 +0530

    home page has been completed

--- Authorize/src/main.jsx
+++ Authorize/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import AppRouter from "./routes/AppRouter.jsx";
+import { store } from "./store/index.js";
+import { Provider } from "react-redux";
+import ErrorBoundary from "./components/common/ErrorBoundary.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <ErrorBoundary>
+      <AppRouter />
+    </ErrorBoundary>
+  </Provider>
+);
